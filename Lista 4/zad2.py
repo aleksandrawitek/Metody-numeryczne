57129240590b13447a9aac9bdf869a55e6f65b68
Aleksandra Witek <olciawitek@wp.pl>
2020-12-02 09:23:40 +0100

    Lista 4

--- Lista 4/zad2.py
+++ Lista 4/zad2.py
@@ -4,8 +4,9 @@ import math
 print("Podaj liczbę: ")
 liczba = input()
 liczba = float(liczba)
-tmp=[1]
+y =[1]
 for x in range(0,6):
-    tmp.append(tmp[x]-(math.pow(tmp[x],5)-liczba)/(5*math.pow(tmp[x],4)))
+    z = y[x]-(math.pow(y[x],5)-liczba)/(5*math.pow(y[x],4))
+    y.append(z)
 print("Pierwiastek 5 stopnia z tej liczby to:")
-print(tmp[6])
+print(y[6])
